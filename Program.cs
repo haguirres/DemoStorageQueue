50e508210cfa5df852043b8e000fc7a528d06ca1
Hector Aguirre <eduardo.aguirre@outlook.com>
2023-12-02 12:01:24 -0600

    InitialCommit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Tracing;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+
+namespace DemoStorageQueue{
+    class Program{
+        static string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=stademoheas;AccountKey=bEjqKC1pb1BP8VR9kOE8Ub6xiNXg/Z0iJtLHX4LTCcZGBUHUpJn6rugf94eN5GqDOwdd02pFXknX+AStvmpSXg==;EndpointSuffix=core.windows.net";
+        static string queueName = "sample-queue";
+        static QueueClient client = default!;
+        static int numMessages = 250;
+        static async Task SendMessages(QueueClient client){
+            await client.CreateIfNotExistsAsync();
+            if(await client.ExistsAsync() == true){
+                for (int i = 1; i <= numMessages; i++)
+                {
+                    await client.SendMessageAsync($"Mensaje no {i} de {numMessages}");
+                    System.Console.WriteLine($"Enviando mensaje {i} de {numMessages}");
+                }
+            }
+        }
+
+        static async Task ReceiveMessages(QueueClient client){
+            for (int i = 1; i <= numMessages; i++)
+            {
+                var message = await client.ReceiveMessageAsync();
+                System.Console.WriteLine($"Procesando: {message.Value.Body}");
+                await client.DeleteMessageAsync(message.Value.MessageId, message.Value.PopReceipt);
+            }
+        }
+
+        static async Task Main(){
+
+            client = new QueueClient(storageConnectionString, queueName);
+            await SendMessages(client);
+            System.Console.WriteLine("Presione una tecla para leer los mensajes");
+            System.Console.ReadKey();
+            await ReceiveMessages(client);         
+        } 
+
+    }
+}
